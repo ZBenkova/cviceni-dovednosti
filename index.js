0afed0f1d9d5cfc76226de609d394f3b414d4708
Zuzana Benkova <zuzanka.dvorska@gmail.com>
2024-03-12 23:56:16 +0100

    nove soubory html a css a js

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-const updateSkill = (id, hodnota) => {
-const hodnota1 = document.querySelector(#)
+const upravProgresBar = (vstupniElement, jmenoTridyCss, hodnota) => {
+    const bar = vstupniElement.querySelector(jmenoTridyCss)
+    bar.style.width = hodnota + "%"
 }
 
+const upravTextovouHodnotu = (vstupniElement, jmenoTridyCss, hodnota) => {
+    const text = vstupniElement.querySelector(jmenoTridyCss)
+    text.innerHTML = hodnota + " / 100"
+}
 
+// function updateSkill(vstupniId, vstupniHodnota) {
+//   Telo funkce
+// }
 
-const HTML = Number(prompt("Zadej svoji úroveň v dovednostech HTML jako číslo 0–100"))
-const CSS1 = Number(prompt("Zadej svoji úroveň v dovednostech CSS jako číslo 0–100"))
-const JS = Number(prompt("Zadej svoji úroveň v dovednostech JavaScript jako číslo 0–100"))
+const updateSkill = (vstupniId, vstupniHodnota) => {
+    // if (vstupniHodnota < 0 || vstupniHodnota > 100) {
+    //     console.log("hodnota mimo rozsah")
+    //     return
+    // }
 
-updateSkill(HTML, )
-updateSkill
-updateSkill
+    // const nalezenyElement = document.querySelector(id)
+    // if (nalezenyElement) {
+    //     const bar = nalezenyElement.querySelector(".skill__progress")
+    //     bar.style.width = value + "%"
+
+    //     const text = nalezenyElement.querySelector(".skill__value")
+    //     text.innerHTML = value + " / 100"
+    // } else {
+    //     console.log("Element se nenasel", id)
+    // }
+
+    const nalezenyElement = document.querySelector(vstupniId)
+    // const bar = nalezenyElement.querySelector(".skill__progress")
+    // bar.style.width = vstupniHodnota + "%"
+    upravProgresBar(nalezenyElement, ".skill__progress", vstupniHodnota)
+
+    // const text = nalezenyElement.querySelector(".skill__value")
+    // text.innerHTML = vstupniHodnota + " / 100"
+    upravTextovouHodnotu(nalezenyElement, ".skill__value", vstupniHodnota)
+}
+
+const HTML = Number(prompt("Vyjadrete svoji uroven v dovednosti HTML jako cislo o hodnote od 0 do 100"))
+const CSSv = Number(prompt("Vyjadrete svoji uroven v dovednosti CSS jako cislo o hodnote od 0 do 100"))
+const JavaScript = Number(prompt("Vyjadrete svoji uroven v dovednosti JavaScript jako cislo o hodnote od 0 do 100"))
+
+updateSkill("#skill1", HTML)
+updateSkill("#skill2", CSSv)
+updateSkill("#skill3", JavaScript)
+
+// updateSkill("#skillNeexistuje", JavaScript)
