66a66ed8283a5cb079619128a098df784f00cf9f
Zuzana Benkova <zuzanka.dvorska@gmail.com>
2024-03-12 18:28:18 +0100

    nove

---
+++ index.js
@@ -0,0 +1,13 @@
+const updateSkill = (id, hodnota) => {
+const hodnota1 = document.querySelector(#)
+}
+
+
+
+const HTML = Number(prompt("Zadej svoji úroveň v dovednostech HTML jako číslo 0–100"))
+const CSS1 = Number(prompt("Zadej svoji úroveň v dovednostech CSS jako číslo 0–100"))
+const JS = Number(prompt("Zadej svoji úroveň v dovednostech JavaScript jako číslo 0–100"))
+
+updateSkill(HTML, )
+updateSkill
+updateSkill
